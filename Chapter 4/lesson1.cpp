d9818794d96fe5c65cc60cecbbcce7e727113677
Vishal <vishalgoswami165342@gmail.com>
2025-02-17 21:40:53 +0530

    Pointers Started

--- Chapter 4/lesson1.cpp
+++ Chapter 4/lesson1.cpp
@@ -1,6 +1,63 @@
 #include<iostream>
 using namespace std;
+
+int pNc(int n , int r){
+
+    int factN = 1;
+    int factR = 1;
+    int factN_R =1;
+
+    for (int i = n; i >= 1; i--)
+    {
+        factN =factN*i;
+    }
+    for(int j =r; j>=1; j--){
+        factR= factR*j;
+    }
+    for(int k =(n-r); k>=1;k--){
+        factN_R = factN_R*k;
+    }
+
+    cout<<"n! = "<<factN<<endl;
+    cout<<"r! = "<<factR<<endl;
+    cout<<"(n-r)! = "<<factN_R<<endl;
+
+
+    return factN/(factR*factN_R);
+}
+
+
+void SwapM1(){
+
+    int a = 10;
+    int b = 20;
+
+    cout<<" a : "<<a;
+    cout<<" b : "<<b;
+    
+    int temp =a;
+    a=b;
+    b=temp;
+    cout<<" a : "<<a;
+    cout<<" b : "<<b;
+}
+
+void SwapM2(){
+
+    int a = 10;
+    int b = 20;
+
+    a = a+b; //10+20 = 30
+    b=a-b;  //30-20 =10
+    a = a-b;//30-10 = 20
+
+    cout<<" a : "<<a;
+    cout<<" b : "<<b;
+}
+
 int main(){
-    cout<<"Hello World";
+    // SwapM1();
+
+SwapM2();
     return 0;
 }
---
+++ Chapter 5/lesson1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+void PointerTest(int& a , int &b){
+    a=0;
+    b=10;
+}
+
+void TestPointer2(int &n ){
+    cout<<"n : "<<&n;
+}
+
+int main(){
+
+//    int a = 10;
+//    int b =20;
+//    cout<<"a : "<<&a<<endl;
+//    cout<<"b : "<<&b<<endl;
+// int a = 10;
+// cout<<"a : "<<&a<<endl;
+// TestPointer2(a);
+ 
+    return 0;
+
+
+}
